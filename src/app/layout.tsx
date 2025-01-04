767a7ce273409ad88e93ebe2d9a0d508c2b040fe
Jide Lambo <jidelambo@gmail.com>
2025-01-04 13:25:26 +0200

    Fix directory structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,24 @@
-import { Providers } from "@/app/providers";
-import "./globals.css";
+// // src/app/(root)/layout.tsx
+// import { Navbar } from '@/components/navbar'
 
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <div className="h-full">
+//       <Navbar />
+//       <main className="md:pt-4">
+//         {children}
+//       </main>
+//     </div>
+//   )
+// }
+
+// src/app/layout.tsx
+import './globals.css'
+import { Providers } from './providers'
 
 export const metadata = {
   title: 'AI Companions',
@@ -18,9 +36,7 @@ export default function RootLayout({
         <script src="https://telegram.org/js/telegram-web-app.js" async />
       </head>
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )

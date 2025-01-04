bc01cd2b8489af2462aa4795d0c4f8edfbe46c56
Jide Lambo <jidelambo@gmail.com>
2025-01-04 13:30:38 +0200

    Fix directory structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,3 @@
-// // src/app/(root)/layout.tsx
-// import { Navbar } from '@/components/navbar'
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <div className="h-full">
-//       <Navbar />
-//       <main className="md:pt-4">
-//         {children}
-//       </main>
-//     </div>
-//   )
-// }
-
 // src/app/layout.tsx
 import './globals.css'
 import { Providers } from './providers'
